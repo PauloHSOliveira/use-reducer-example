2ccbeb685cec79ece70fbb97c0349f521a72c875
paulooliveira <paulooliveiradev@gmail.com>
2021-07-29 14:53:52 -0300

    chore: create form component

---
+++ src/components/form/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Input, Button, Form} from 'antd'
+
+const FormFC:React.FC = () => {
+
+    return (
+        <Form>
+            <Input />
+            <Input />
+            <Button>Teste</Button>
+        </Form>
+    )
+}
+
+export {FormFC}
