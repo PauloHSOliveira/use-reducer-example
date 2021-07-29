fe824f5d8f8ab13957ad3b2d157069d865490e4d
paulooliveira <paulooliveiradev@gmail.com>
2021-07-29 15:13:20 -0300

    styles: added container and content styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import {FormFC} from '@/components/form'
+import {FormFC} from './components/form'
+
+import './App.css'
 
 function App() {
   return (
-    <div className="container">
-      <div className="content">
+    <div className="container" style={{height:'100vh', width:'100wh', backgroundColor:'#000', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
+      <div className="content" style={{backgroundColor:'#fff', borderRadius:'4px', height:'auto', width:'400px', padding:'15px'}}>
         <FormFC />
       </div>
     </div>
