430b9f05691738a14a3d3d791d03122b2a8cffad
paulooliveira <paulooliveiradev@gmail.com>
2021-07-29 17:58:11 -0300

    refactor: create styles file

---
+++ src/GlobalStyles.ts
@@ -0,0 +1,26 @@
+import {CSSProperties} from 'react'
+
+const container_styles:CSSProperties = {
+    height:'100vh', 
+    width:'100wh', 
+    backgroundColor:'#000', 
+    display:'flex', 
+    flexDirection:'column', 
+    justifyContent:'center', 
+    alignItems:'center'
+}
+
+const content_styles:CSSProperties = {
+    height:'100vh', 
+    width:'100wh', 
+    backgroundColor:'#000', 
+    display:'flex', 
+    flexDirection:'column', 
+    justifyContent:'center', 
+    alignItems:'center'
+}
+
+export {
+    container_styles,
+    content_styles
+}
---
+++ src/components/form/styles.ts
@@ -0,0 +1,22 @@
+import {CSSProperties} from 'react'
+
+const button_styles:CSSProperties = {
+    marginTop:'10px'
+}
+
+const input_styles:CSSProperties = {
+    marginTop:'20px'
+}
+
+const form_styles:CSSProperties = {
+    display:'flex', 
+    flexDirection:'column', 
+    alignItems:'center', 
+    justifyContent:'center'
+}
+
+export {
+    button_styles,
+    input_styles,
+    form_styles
+}
